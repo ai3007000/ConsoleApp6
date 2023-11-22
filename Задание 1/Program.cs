43c05d860aeb0f80ce414f6dd8c22a13dfa6803e
Артём Иванов <ai3007000@yandex.ru>
2023-11-22 19:55:47 +0700

    Добавьте файлы проекта.

---
+++ Задание 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp6.Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Задание 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp6.Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = @"C:\Users\ai500\Desktop\Прогромирование";
+                string[] directories = Directory.GetDirectories(path);
+                foreach (var item in directories)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
