8f1b8ff1f32fc11009446e7d0b40b2a622612226
Артём Иванов <ai3007000@yandex.ru>
2023-11-22 21:48:32 +0700

    Задание 2 ошибка

--- Задание 2/Program.cs
+++ Задание 2/Program.cs
@@ -5,25 +5,62 @@ namespace ConsoleApp6.Test2
 {
     class Program
     {
-        static void Main(string[] args)
+        static public void Main()
         {
             try
             {
-                string path = @"D:\Книги\Прогромирование";
-                if (Directory.Exists(path))
+                DriveInfo[] drives = DriveInfo.GetDrives();
+                Console.WriteLine("Список логических дисков получен.");
+                foreach (var drive in drives)
                 {
-                    // if (Directory.GetLastAccessTime(path). < TimeSpan.FromMinutes(30))
-                    DateTime time = DateTime.Now;
-                    TimeSpan tm = new TimeSpan(0, time.Minute, 0);
-
-                    TimeSpan TimeLastAccess = new TimeSpan(0, Directory.GetLastAccessTime(path).Minute, 0);
-                    Console.WriteLine(TimeLastAccess < TimeSpan.FromMinutes(30));
+                    if (drive.IsReady)
+                    {
+                        var Folder = Directory.GetDirectories(drive.Name);// Список каталогов
+                        // C:\$WinREAgent
+                        foreach (var fd in Folder)
+                        {
+                            long TotalFolderSize = FolderSize(fd); // Расчёт размера файлов
+                            Console.WriteLine($"Общий размер файлов: {FolderSize(fd)}");
+                        }
+                    }
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
+        }
+        /// <summary>
+        /// Функция расчёта размера файлов
+        /// </summary>
+        /// <param name="path">Путь</param>
+        /// <returns>Размер всех файлов</returns>
+        static long FolderSize(string path)
+        {
+            long totalSizeOfDir = 0; // Размер файлов
+
+            var allFiles = Directory.GetFiles(path); // Каталоги
+            if (allFiles.Length > 0)
+            {
+                Console.WriteLine("Список файлов получен." + allFiles[0]);
+                foreach (var file in allFiles)
+                {
+                    if (File.Exists(file))
+                    {
+                        Console.WriteLine("Файл существует.");
+                        totalSizeOfDir += file.Length; // Расчёт размера файлов
+                    }
+                }
+            }
+
+            var subFolders = Directory.GetDirectories(path); // Подкаталоги
+
+            foreach (var dir in subFolders)
+            {
+                totalSizeOfDir += FolderSize(dir); // Рекурсия
+            }
+            return totalSizeOfDir;
+
         }
     }
 }
