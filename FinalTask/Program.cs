bcdea2937eeffc26ae9548c3e8bc75000a1431f2
Артём Иванов <ai3007000@yandex.ru>
2023-11-23 22:19:25 +0700

    Финальное задание проблема с десериализацией

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,18 +1,39 @@
 ﻿using System;
 using System.Text;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
 
 namespace ConsoleApp6.FinalTask
 {
+    class Student
+    {
+        public string? Name { get; set; }
+        public string? Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public override string ToString()
+        {
+            return $"Имя: {this.Name}\tГруппа: {this.Group}\tДата рождения: {this.DateOfBirth}";
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            BinaryFormatter formatter = new BinaryFormatter();
             const string path = @"D:\Програмирование\C#\ConsoleApp6\FinalTask\Students.dat";
-            using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
+            if (File.Exists(path))
             {
-                var str = reader.ReadString();
-                Console.WriteLine(str);
+                using (var file = new FileStream(path, FileMode.OpenOrCreate))
+                {
+                    
+                    Student[] humanArray = (Student[])formatter.Deserialize(file);
+                    Console.WriteLine("Десериализация выполнена.");
+                    foreach(var student in humanArray)
+                    {
+                        Console.WriteLine(student.ToString());
+                    }
+                }
             }
         }
     }
