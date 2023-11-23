0f3a6922395a7a71f5d245209f0447298e662e1e
Артём Иванов <ai3007000@yandex.ru>
2023-11-23 20:48:30 +0700

    Задание 4 проблема

---
+++ FinalTask/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp6.FinalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string path = @"D:\Програмирование\C#\ConsoleApp6\FinalTask\bin\Debug\net6.0\Students.dat";
+            using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
+            {
+                var str = reader.ReadString();
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
